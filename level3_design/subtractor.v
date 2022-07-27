0740c46031bebeb14c61ddd24dfe6a89e315f1a0
Paripati_Yeshwanth <74342939+Yesh1210@users.noreply.github.com>
2022-07-27 17:39:50 +0530

    inserting bug as multiplication instead of subtraction

--- level3_design/subtractor.v
+++ level3_design/subtractor.v
@@ -5,6 +5,6 @@ module subtract(in1 , in2 , out_sub);
   input signed [4:0]in2;
   output signed [8:0]out_sub;
   
-  assign out_sub = in1 - in2;
+  assign out_sub = in1 * in2;     //out_sub = in1 - in2
   
 endmodule
