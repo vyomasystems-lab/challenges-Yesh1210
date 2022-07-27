2e37c2583ce92cc7b667d5e8e98e80044682cdea
Paripati_Yeshwanth <74342939+Yesh1210@users.noreply.github.com>
2022-07-27 17:38:57 +0530

    Inserting bug as subtraction instead of addition

--- level3_design/adder.v
+++ level3_design/adder.v
@@ -5,6 +5,6 @@ module add(in1 , in2 , out_add);
   input signed [4:0]in2;
   output signed [8:0]out_add;
   
-  assign out_add = in1 + in2;
+  assign out_add = in1 - in2;    //out_add = in1 + in2
   
 endmodule
