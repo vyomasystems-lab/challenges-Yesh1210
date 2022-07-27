ecb28414d850446e219ed66a93017ec57332403e
Paripati_Yeshwanth <74342939+Yesh1210@users.noreply.github.com>
2022-07-27 17:37:34 +0530

    updating testbench file

--- level3_design/alu_testbench.py
+++ level3_design/alu_testbench.py
@@ -19,8 +19,8 @@ async def test1(dut):
     dut.rstn.value = 1
     await FallingEdge(dut.clk)
     
-    A = 5
-    B = 10
+    A = 10
+    B = 5
     C = 0
     # input driving
     dut.in1.value = A
@@ -30,7 +30,6 @@ async def test1(dut):
 
     dut._log.info(f'A={A:05} B={B:05} C={C:05} model={A+B:05} DUT={int(dut.out_top.value):05}')
     assert dut.out_top.value == A+B, "Test1 failed with: in1={A} , in2={B} , opcode={C} , DUT_OUT={D}".format(A=dut.in1.value, B=dut.in2.value, C=dut.opcode.value, D=dut.out_top.value)
-
 
 @cocotb.test()
 async def test2(dut):
